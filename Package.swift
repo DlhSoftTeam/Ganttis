5ba8f2a717beeca8232f57fcdd6a601a45bffb0b
DlhSoft <Contact@DlhSoft.com>
2022-04-03 18:50:06 +0300

    Updated framework

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Ganttis",
-                      url: "https://github.com/DlhSoftTeam/Ganttis/releases/download/artifacts-2022-03-18/Ganttis.xcframework.2.1.12.zip",
-                      checksum: "272a6b5f62ae6cf4a4344a2b13e59f18a1100419ba497e2e26979243af8d2259"),
+                      url: "https://github.com/DlhSoftTeam/Ganttis/releases/download/artifacts-2022-04-03/Ganttis.xcframework.2.1.13.zip",
+                      checksum: "a586dc04ae72aab6eeb326c2c910b07822299d43db88d4bde3f6596f0a4541a2"),
     ]
 )

03efd40f134d9c4a6b5cacfc7a20b92af1e96281
DlhSoft <Contact@DlhSoft.com>
2022-05-21 22:27:31 +0300

    +

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Ganttis",
-                      url: "https://github.com/DlhSoftTeam/Ganttis/releases/download/artifacts-2022-05-21/Ganttis.xcframework.2.2.zip",
+                      url: "https://github.com/DlhSoftTeam/Ganttis/releases/download/2022-05-21/Ganttis.xcframework.2.2.zip",
                       checksum: "5ed7741af75d179f1f23dcbb2c1c09e7fbfe9ed16cb470433c3ce8f8f52b0f83"),
     ]
 )
